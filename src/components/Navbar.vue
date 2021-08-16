a75a3e04b8962fb7de274eb0dbd7388629349a7b
Abhijeet Jain <abhijeetjain456@gmail.com>
2021-08-17 04:40:25 +0530

    route fix Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Testing Libraries</a>
+  <a class="navbar-brand" href="/testing-libraries-v2/">Testing Libraries</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
